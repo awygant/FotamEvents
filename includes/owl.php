7a379cf9aeb743f492b8abc79aa11e01bc0f49bd
Anna Wygant <anna.wygant@gmail.com>
2015-06-08 14:05:01 -0700

    Owl Carousel
    More features than jQuery UI.
    Docs are here:
    http://owlgraphic.com/owlcarousel/

---
+++ includes/owl.php
@@ -0,0 +1,8 @@
+<!-- Important Owl stylesheet -->
+<link rel="stylesheet" href="owl.carousel/owl-carousel/owl.carousel.css">
+
+<!-- Default Theme -->
+<link rel="stylesheet" href="owl.carousel/owl-carousel/owl.theme.css">
+
+<!-- Include js plugin -->
+<script src="owl.carousel/owl-carousel/owl.carousel.js"></script>
--- photo.php
+++ photo.php
@@ -9,6 +9,38 @@
     <title>Palm Trees & Chickens</title>
     <?php include_once("includes/meta.php");?>
     <?php include_once("includes/resources.php"); ?>
+    <?php include_once("includes/owl.php"); ?>
+    <script type = "text/javascript">
+        $(document).ready(function() {
+
+            carousel = $("#carousel");
+
+            carousel.owlCarousel({
+                singleItem: true,
+                afterMove: navigate
+            });
+
+            owl = carousel.data('owlCarousel');
+            owl.jumpTo(1);
+
+            $(".goRight").click(function(){
+                carousel.trigger('owl.next');
+            });
+            $(".goLeft").click(function(){
+                carousel.trigger('owl.prev');
+            });
+
+            function navigate(){
+                if(owl.currentItem == 0){
+                    // Navigate to the left (refresh the viewer to feature the left item)
+                }
+                else if(owl.currentItem == 2){
+                    // Navigate to the right
+                }
+            }
+        })
+    </script>
+
 </head>
 <body>
 <!-- TODO: make the background image for this class dynamic. On this page, it should match the img from .mainPhoto -->
@@ -30,43 +62,64 @@
 
     <div id = "wrap">
 
-        <!-- TODO: If user came here from a gallery: include both fauxCards and implement swipe navigation (swipe left to right = go to previous, vice versa) -->
-        <!-- TODO: Make clicking this go to previous photo from the gallery. -->
-        <div class = "card fauxCard fauxCardPrevious glassy">
-            <div class = "descriptionBox">
+
+
+
+        <div id = "carousel">
+            <!-- TODO: If user came here from a gallery: include both fauxCards and implement swipe navigation (swipe left to right = go to previous, vice versa) -->
+            <!-- TODO: Make clicking this go to previous photo from the gallery. -->
+            <div class = "card large-card-blurry glassy">
                 <!-- TODO: Make this pull the previous gallery image. -->
-                <img class = "blurry" src = "http://flashfotoapi.com/api/get/3401921?partner_username=facebook&partner_apikey=apikey4facebook&resize=fit&height=200"/>
+                <div class = "blurryWrapper">
+                    <img class = "blurry" src = "http://flashfotoapi.com/api/get/3401921?partner_username=facebook&partner_apikey=apikey4facebook&resize=fit&height=200"/>
+                </div>
+                <div class = "descriptionBox">
+                    <div class = "gearOne spinner reverseSpinner"></div>
+                    <div class = "gearTwo spinner"></div>
+                    <div class = "gearThree spinner reverseSpinner"></div>
+                </div>
             </div>
 
-        </div>
-
-        <div class = "card large-card glassy">
-            <img src = "http://events.fotam.com/img/featured/1"/>
-            <div class = "descriptionBox">
-                <div>
-                    <img class = "userProfilePic" src = "img/examples/archie.png" alt = "Firstname Lastname" />
-                    <div class = "userInfoText">
-                        <p class = "username">DIDDLEYDOODLE</p>
-                        <p class= "photoMetaTimestamp">March 10, 2015 7:03 PM</p>
+            <div>
+                <button class = "glassy photoNav goLeft"><span class = "carrot carrot-left"></span></button>
+                <div class = "card large-card glassy">
+                    <img src = "http://flashfotoapi.com/api/get/3400747?partner_username=facebook&partner_apikey=apikey4facebook"/>
+                    <div class = "descriptionBox">
+                        <div>
+                            <img class = "userProfilePic" src = "img/examples/archie.png" alt = "Firstname Lastname" />
+                            <div class = "userInfoText">
+                                <p class = "username">DIDDLEYDOODLE</p>
+                                <p class= "photoMetaTimestamp">March 10, 2015 7:03 PM</p>
+                            </div>
+                        </div>
+                        <div class = "socialStat socialStatFB"><span class = "bold uppercase">102</span>Likes</div>
+                        <div class = "socialStat socialStatShare"><span class = "bold uppercase">3</span>Shares</div>
+                        <div class = "socialStat socialStatTW"><span class = "bold uppercase">44</span>Tweets</div>
+                        <button class = "shareButton fbShareButton bold uppercase">Facebook Post</button>
+                        <button class = "shareButton twShareButton bold uppercase">Tweet</button>
                     </div>
                 </div>
-                <div class = "socialStat socialStatFB"><span class = "bold uppercase">102</span>Likes</div>
-                <div class = "socialStat socialStatShare"><span class = "bold uppercase">3</span>Shares</div>
-                <div class = "socialStat socialStatTW"><span class = "bold uppercase">44</span>Tweets</div>
-                <button class = "shareButton fbShareButton bold uppercase">Facebook Post</button>
-                <button class = "shareButton twShareButton bold uppercase">Tweet</button>
+                <button class = "glassy photoNav goRight"><span class = "carrot carrot-right"></span></button>
+            </div>
+
+
+            <!-- TODO: Make clicking this advance the gallery. -->
+            <div class = "card large-card-blurry glassy">
+                <!-- TODO: Make this pull the next gallery image. -->
+                <div class = "blurryWrapper">
+                    <img class = "blurry" src = "http://flashfotoapi.com/api/get/3406732?partner_username=facebook&partner_apikey=apikey4facebook&resize=fit&height=200"/>
+                </div>
+                <div class = "descriptionBox">
+                    <div class = "gearOne spinner reverseSpinner"></div>
+                    <div class = "gearTwo spinner"></div>
+                    <div class = "gearThree spinner reverseSpinner"></div>
+                </div>
             </div>
         </div>
 
-        <!-- TODO: Make clicking this advance the gallery. -->
-        <div class = "card fauxCard fauxCardNext glassy">
-            <div class = "descriptionBox">
-                <!-- TODO: Make this pull the next gallery image. -->
-                <img class = "blurry" src = "http://flashfotoapi.com/api/get/3406732?partner_username=facebook&partner_apikey=apikey4facebook&resize=fit&height=200"/>
-            </div>
-        </div>
 
     </div>
+
 
 
 
